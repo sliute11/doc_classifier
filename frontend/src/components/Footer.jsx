cd7d0cf718344ffd52f17067d34211d1d6c713d0
sliute11 <stefan.liute@endava.com>
2025-05-31 16:09:37 +0300

    Styled the page using tailwindcss

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="w-full p-4 text-center text-xs border-t border-cyan-700 bg-gray-950 text-cyan-500 font-mono">
+      <p>&copy; 2025 Spartans Team · Doc Classifier Project</p>
+    </footer>
+  );
+}
+
+export default Footer;
